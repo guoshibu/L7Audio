84c50d3ec6295e6a7b35225026f192ebdc0493a4
L7AudioDeveloper <user@example.com>
2026-07-02 23:19:08 +0800

    chore: 版本号更新至 v1.4.4 (versionCode: 44)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.aug32.l7audio"
         minSdk = 30
         targetSdk = 30
-        versionCode = 43
-        versionName = "1.4.3"
+        versionCode = 44
+        versionName = "1.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
